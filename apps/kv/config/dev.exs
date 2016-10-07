7b8e725eefcf099632873ef04e85ffde376636a8
Justin Zimmer <jtzimmer6@gmail.com>
2016-10-07 16:48:47 -0700

    Committing completed KvUmbrella app per elixir-lang.org OTP instructional.

---
+++ apps/kv/config/dev.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+config :kv, :routing_table,
+       [{?a..?z, :"foo@Justins-MacBook-Pro"}]
---
+++ apps/kv/config/prod.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+config :kv, :routing_table,
+        [{?a..?m, :"foo@Justins-MacBook-Pro"},
+         {?n..?z, :"bar@Justins-MacBook-Pro"}]
